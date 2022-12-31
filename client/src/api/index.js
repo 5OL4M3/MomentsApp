50841654c66583e2e45a061be2baeeb58c86657e
Kevin Lin <klin0732@gmail.com>
2022-12-30 21:02:02 -0500

    update for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'https://mymomentsapp-7jzf.onrender.com/posts'});
+const API = axios.create({baseURL: 'https://mymomentsapp-7jzf.onrender.com'});
 //const API = axios.create({baseURL: 'http://localhost:5000/'});
 
 API.interceptors.request.use((req) => {
